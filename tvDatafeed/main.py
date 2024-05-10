092789050215ec8be6c5349f4a00337883c01a0b
juliansimon <julian.simon@gmail.com>
2024-05-10 14:48:29 +0200

    Add close connection

--- tvDatafeed/main.py
+++ tvDatafeed/main.py
@@ -87,6 +87,11 @@ class TvDatafeed:
             "wss://data.tradingview.com/socket.io/websocket", headers=self.__ws_headers, timeout=self.__ws_timeout
         )
 
+    def __close_connection(self):
+        logging.info("close websocket connection")
+        ret = self.ws.close()
+        logging.info(ret)
+
     @staticmethod
     def __filter_raw_message(text):
         try:
@@ -287,7 +292,16 @@ class TvDatafeed:
             if "series_completed" in result:
                 break
 
+
+        # Close connection
+        self.__close_connection()
+
         return self.__create_df(raw_data, symbol)
+
+        
+    #def close_connection():
+    #    self.__close_connection()
+
 
     def search_symbol(self, text: str, exchange: str = ''):
         url = self.__search_url.format(text, exchange)
